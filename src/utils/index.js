044f9b6709d157cd2abf6369d40fd1d7fc6b4225
MohammedGouse01 <gousetaj001@gmail.com>
2023-08-09 05:58:10 +0530

    defined debounce and mount hooks

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,25 @@
 import React from "react"
+import { useEffect, useState } from 'react';
 
 export const cleanObject=(obj)=>{
     const filteredEntries=Object.entries(obj).filter(([key, value])=>value !== undefined && value !=='')
-    return Object.fromEntries(filteredEntries);
+    return Object.fromEntries(filteredEntries)
+}
+
+export const useMount = (callback)=>{
+        useEffect(()=>{
+            callback();
+        },[])
+
+}
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebouncedValue(value), delay);
+        return () => clearTimeout(timeout);
+    }, [value, delay]);
+
+    return debouncedValue;
 }
